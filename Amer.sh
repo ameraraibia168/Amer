6805027248ad38413f8b4e3a4436dd1ff168f694
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2019-02-07 21:53:46 +0100

    Create Amer.sh

---
+++ Amer.sh
@@ -0,0 +1,211 @@
+
+g='\033[1;32m'
+
+p='\033[1;35m'
+
+cd
+
+clear
+
+termux-setup-storage
+
+rm -rf $HOME/payload5.zip
+
+rm -rf $HOME/payload5
+
+rm -rf $HOME/../usr/bin/payload.sh
+
+rm -rf $HOME/../usr/bin/payload
+
+rm -rf $HOME/../usr/bin/setup5.sh
+
+echo -e "$g+++++++++++++++>$p[P**********]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Pl*********]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Ple********]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Plea*******]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Pleas******]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please*****]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please ****]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please W***]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please Wa**]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please Wai*]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+clear
+
+
+
+
+
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install git -y
+
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install figlet -y
+
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install python -y
+
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install python2 -y
+
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install python3 -y
+
+pkg install php -y
+
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install nano -y
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install curl -y
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+
+mkdir /sdcard/payload5
+
+mkdir /sdcard/payload5/Encrypt
+
+git clone https://github.com/amerlaceset/payload5
+
+
+cp payload5/payload5.zip $HOME
+
+
+rm -rf payload5
+
+
+unzip payload5.zip
+
+
+cd payload5 && chmod +x *
+
+cd $HOME/payload5
+
+chmod +x .*
+
+cd
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload.sh
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload
+
+cp $HOME/payload5/.setup5.sh $HOME/../usr/bin/setup5.sh
+
+chmod +x $HOME/../usr/bin/setup5.sh
+
+cd 
+
+rm -rf Amer
+
+cd $HOME/payload5/.tool
+
+cd
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload.sh
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload5
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/Amerr
+
+cp $HOME/payload5/.setup5.sh $HOME/../usr/bin/setup5.sh
+
+chmod +x $HOME/../usr/bin/setup5.sh
+
+cd $HOME/payload5/.tool
+
+chmod +x */*
+
+clear
+
+
+echo " Hello "
+
+echo -e " $p open ---> $g payload.sh "
+
+echo -e " $g payload5 "
+
+sleep 2
+
+payload
