f18bbd5bdc38db64d0d3cf1521e03e47093e008e
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2019-03-11 21:28:58 +0100

    Create Amer.sh

---
+++ Amer.sh
@@ -0,0 +1,220 @@
+
+g='\033[1;32m'
+
+p='\033[1;35m'
+
+cd
+
+clear
+
+termux-setup-storage
+
+rm -rf $HOME/Virus4.zip
+
+rm -rf $HOME/Virus4
+
+rm -rf $HOME/../usr/bin/Virus4.sh
+
+rm -rf $HOME/../usr/bin/Virus
+
+rm -rf $HOME/../usr/bin/virus
+
+rm -rf $HOME/../usr/bin/lovemam.sh
+
+echo -e "$g+++++++++++++++>$p[P**********]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Pl*********]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Ple********]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Plea*******]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Pleas******]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please*****]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please ****]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please W***]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please Wa**]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please Wai*]$g<+++++++++++++++++"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+clear
+
+
+
+
+
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install git -y
+
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install figlet -y
+
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install python -y
+
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install python2 -y
+
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install python3 -y
+
+pkg install php -y
+
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install nano -y
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+pkg install curl -y
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<+++++++++++++++++"
+
+
+mkdir /sdcard/Virus4
+
+mkdir /sdcard/Virus4/Encrypt
+
+git clone https://github.com/amerlaceset/payload6
+
+cd
+
+cd payload6
+
+mv -v Virus4.zip $HOME
+
+rm -rf payload6
+
+cp Virus4/Virus4.zip $HOME
+
+
+rm -rf payload6
+
+
+unzip Virus4.zip
+
+
+cd Virus4 && chmod +x *
+
+cd $HOME/Virus4
+
+chmod +x .*
+
+cd
+
+cp $HOME/Virus4/Virus4.sh $HOME/../usr/bin/Virus4.sh
+
+cp $HOME/Virus4/Virus4.sh $HOME/../usr/bin/Virus
+
+cp $HOME/Virus4/.lovemam.sh $HOME/../usr/bin/lovemam.sh
+
+chmod +x $HOME/../usr/bin/lovemam.sh
+
+cd 
+
+rm -rf Amer
+
+cd $HOME/Virus4/.tools
+
+cd
+
+cp $HOME/Virus4/Virus4.sh $HOME/../usr/bin/Virus4.sh
+
+cp $HOME/Virus4/Virus4.sh $HOME/../usr/bin/Virus
+
+cp $HOME/Virus4/Virus4.sh $HOME/../usr/bin/Virus4
+
+cp $HOME/Virus4/Virus4.sh $HOME/../usr/bin/virus
+
+cp $HOME/Virus4/.lovemam.sh $HOME/../usr/bin/lovemam.sh
+
+chmod +x $HOME/../usr/bin/lovemam.sh
+
+cd $HOME/Virus4/.tools
+
+chmod +x */*
+
+clear
+
+
+echo " Hello "
+
+echo -e " $p open ---> $g Virus4.sh "
+
+echo -e " $g Virus4 "
+
+sleep 2
+
+Virus
