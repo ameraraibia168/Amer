96bc2706e192a2def65a3f7c6f9702b114b197f8
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2019-04-04 20:53:05 +0100

    Add files via upload

---
+++ Amer.sh
@@ -0,0 +1,106 @@
+
+g='\033[1;32m'
+
+p='\033[1;35m'
+
+cd
+clear
+termux-setup-storage
+rm -rf $HOME/Virus
+rm -rf $HOME/Virus4.zip
+rm -rf $HOME/Virus4
+rm -rf $HOME/../usr/bin/Virus4.py
+rm -rf $HOME/../usr/bin/Virus
+rm -rf $HOME/../usr/bin/virus
+clear
+echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'-->            '$g']/'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+sleep 0.6
+clear
+echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
+sleep 0.6
+clear
+|echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+sleep 0.6
+clear
+echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+sleep 0.5
+clear
+echo -e $g 'Please Wait ===+['$p'----------->   '$g']/'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+sleep 0.4
+clear
+echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+sleep 0.4
+clear
+
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+sleep 0.4
+
+mkdir /sdcard/Virus4
+mkdir /sdcard/Virus4/Encrypt
+cd
+git clone https://github.com/amerlaceset/Virus
+cd
+cd Virus
+mv -v Virus4.zip $HOME
+cd
+rm -rf Virus
+cd
+unzip Virus4.zip
+cd Virus4 && chmod +x *
+cd $HOME/Virus4
+chmod +x *
+cd
+cp $HOME/Virus4/Virus4.py $HOME/../usr/bin/Virus4.py
+cp $HOME/Virus4/Virus4.py $HOME/../usr/bin/Virus
+cd
+rm -rf Virus
+rm -rf Virus4.zip
+cd $HOME/Virus4/.tools
+cd
+cp $HOME/Virus4/Virus4.py $HOME/../usr/bin/Virus4.py
+cp $HOME/Virus4/Virus4.py $HOME/../usr/bin/Virus
+cp $HOME/Virus4/Virus4.py $HOME/../usr/bin/Virus4
+cp $HOME/Virus4/Virus4.py $HOME/../usr/bin/virus
+
+cd $HOME/Virus4/.tools
+chmod +x */*
+clear
+echo -e "$g+++++++++++>[$pWelcome to the new update$p$g]<+++++++++++++"
+echo -e "     Hello      "
+echo -e "     $p     new "
+echo -e "     $g         Update "
+sleep 2
+cd $HOME/Virus4
+python2 Virus4.py
+
+
